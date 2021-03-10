c46cc761e8bd78910ce393536ba2894912b9b3a3
testwhitebaby <maybezhmn@163.com>
2021-03-10 22:00:15 +0800

    完成

--- hogwards_apitest/api.py
+++ hogwards_apitest/api.py
@@ -1,16 +1,25 @@
 import requests
 
+session = requests.sessions.Session()
 
 class BaseApi(object):
     method = "GET"
     url = ""
     params = {}
+    cookies = {}
     headers = {}
     data = {}
     json = {}
 
+    def __init__(self):
+        self.response() == None
+
     def set_params(self, **params):
         self.params = params
+        return self
+
+    def set_cookie(self, key, value):
+        self.cookies.update({key, value})
         return self
 
     def set_data(self, data):
@@ -22,18 +31,46 @@ class BaseApi(object):
         return self
 
     def run(self):
-        self.response = requests.request(
+        self.response = session.request(
             self.method,
             self.url,
             params=self.params,
+            cookies=self.cookies,
             headers=self.headers,
             data=self.data,
             json=self.json
         )
         return self
 
+    def extract(self,field):
+        value = self.response
+        for _key in field.split("."):
+            if isinstance(value, requests.Response):
+                if _key == "json()":
+                    value = self.response.json()
+                else:
+                    value = getattr(value, _key)
+            elif isinstance(value, (requests.structures.CaseInsensitiveDict, dict)):
+                value = value[_key]
+
+        return value
+
+    # jsonpath格式去返回值，支持headers,body
     def validate(self, key, expected_value):
-        actual_value = getattr(self.response, key)
+        # value = self.response
+        # for _key in key.split("."):
+        #     if isinstance(value,requests.Response):
+        #         if _key == "json()":
+        #             value = self.response.json()
+        #         else:
+        #             value = getattr(value,_key)
+        #     elif isinstance(value, (requests.structures.CaseInsensitiveDict,dict)):
+        #         value = value[_key]
+        # assert value == expected_value
+        # return self
+        actual_value = self.extract(key)
         assert actual_value == expected_value
         return self
 
+    def get_response(self):
+        return self.response()
--- tests/test_core.py
+++ tests/test_core.py
@@ -14,6 +14,32 @@ class ApiHttpbinPost(BaseApi):
     data = "abc=123"
     json = {"abc":123}
 
+class ApiHttpbinGetCookies(BaseApi):
+    url = "http://httpbin.org/cookies"
+    params = {}
+    method = "GET"
+    headers = {"accept":"application/json"}
+
 def test_httpbin_get():
     ApiHttpbinGet().run()\
-        .validate("status_code",200)
+        .validate("status_code",200)\
+        .validate("headers.server","gunicorn/19.9.0") \
+        .validate("json().url", "http://httpbin.org/get")
+
+def test_httpbin_setcookies():
+    api_run = ApiHttpbinGetCookies()\
+                .set_cookie("freeform1", "123")\
+                .set_cookie("freeform2", "456")\
+                .run()
+    freeform1 = api_run.extract("json().cookies.freeform1")
+    freeform2 = api_run.extract("json().cookies.freeform2")
+    assert freeform1 == "123"
+    assert freeform2 == "456"
+
+def test_httpbin_parameters_extract():
+    # 取值
+    freeform = ApiHttpbinGetCookies()\
+                .set_cookie("freeform","123")\
+                .run()\
+                .extract("json().cookies.freeform")
+    assert freeform == "123"
